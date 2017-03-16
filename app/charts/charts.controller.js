0b2dcba5538e411d31884281d188caff3f3765f3
Arlin <arlin.grijalba@gmail.com>
2017-03-16 10:13:49 -0700

    Fist chart appearing.

--- app/charts/charts.controller.js
+++ app/charts/charts.controller.js
@@ -9,7 +9,7 @@
 
     /* @ngInject */
     function ChartsController(inqstatsFactory) {
-
+        var vm = this;
         inqstatsFactory
             .getData("population")
             .then(function(data) {
@@ -26,26 +26,23 @@
                 console.log(error);
             })
 
-        /* -- STEP 4 -- */
-
         /* -- STEP 5: GIVE THE DATA TO ANGULAR CHART -->
-
         Done that? Great!
-
         Next up, for Angular Chart, you only need to use the `chart-data` and
         `chart-label` directives in order to get one up and running.
-
         You'll need a data structure like this to house the massaged data from step 2.
-
         ```js
         vm.bigMac = {
           data: [4.4, 4.1, 3.8] // while literal in this example, this will really be the result of the call to the .map function.
           labels: ['2014', '2015', '2016'] // see above comment, same applys here.
         }
         ```
-
         Once you've finished all this, you can move to ../index.html for Step 6 - building the UI!
         */
 
+        vm.bigMac = {
+            data: [4.4, 4.1, 3.8], // while literal in this example, this will really be the result of the call to the .map function.
+            labels: ['2014', '2015', '2016'] // see above comment, same applys here.
+        }
     }
 })();
